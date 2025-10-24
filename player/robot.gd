cf24162fc1e27137b1dba971d04bcc566ecea2d1
Adam Giles <adamag@cox.net>
2025-10-23 22:50:49 -0400

    init

---
+++ player/robot.gd
@@ -0,0 +1,71 @@
+extends CharacterBody3D
+
+
+var max_velocity: float = 5.0
+var current_speed: float = 0.0
+var move_speed: float = 5.0
+var turn_speed: float = .3
+var damping_force: float = 10.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	var command := get_command()
+	
+	# handle action
+	if command.action:
+		attempt_action()
+	
+	# handle rotation
+	if command.left:
+		rotate_y(delta * turn_speed)
+	elif command.right:
+		rotate_y(-delta * turn_speed)
+	
+	# handle movement
+	if command.forwards:
+		current_speed = clampf(current_speed + delta * move_speed, -max_velocity, max_velocity)
+	elif command.backwards:
+		current_speed = clampf(current_speed - delta * move_speed, -max_velocity, max_velocity)
+	else:
+		current_speed = move_toward(current_speed, 0, delta * damping_force)
+	print("current_speed: ", current_speed)
+	print("velocity is: ", velocity)
+	print("rot: ", rotation)
+	var adj_vel := Vector3(0, 0, -current_speed).rotated(Vector3.UP, rotation.y)
+	adj_vel = adj_vel
+	print("rot-adj: ", adj_vel)
+	velocity = adj_vel
+
+	move_and_slide()
+	
+	animate_treads(delta, command)
+	
+	$AnimatableBody.global_position = self.global_position
+
+func attempt_action() -> void:
+	print("using action")
+
+func animate_treads(delta: float, command: PlayerCommand) -> void:
+	var l_forwards := true
+	var r_forwards := true
+	if command.left:
+		l_forwards = false
+	elif command.right:
+		r_forwards = false
+	var rotate_speed = current_speed
+	if command.has_rotation():
+		rotate_speed = 3.0
+	$Treads/Left.rotate_x(-delta * rotate_speed * (1 if l_forwards else -1))
+	$Treads/Right.rotate_x(-delta * rotate_speed * (1 if r_forwards else -1))
+
+func get_command() -> PlayerCommand:
+	var command := PlayerCommand.new()
+	command.action = Input.is_action_pressed("action")
+	command.forwards = Input.get_action_strength("forward")
+	command.backwards = Input.get_action_strength("backward")
+	command.right = Input.get_action_strength("right")
+	command.left = Input.get_action_strength("left")
+	return command
